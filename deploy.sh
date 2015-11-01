3cc67fba9cb3601120aa8bdeddb66af1a006284d
makotot <makoto.ttn@gmail.com>
2015-11-01 19:49:36 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -15,4 +15,6 @@ git config --list
 echo -e "---\nbranches:\n  only:\n    - dev" > .travis.yml
 git add .
 git commit -m "Deploy to gh-pages"
+git log --oneline
+git branch -a
 git push -fq "https://${GH_TOKEN}@github.com/${GH_REPO}.git" origin master > /dev/null 2>&1
